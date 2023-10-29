89fb9db7185c36a7cdabc568e70bed9b453e9990
CriiXpsitos <cristianpee2609@gmail.com>
2023-10-28 23:57:02 -0500

    .

--- IntroduccionJS/IntroduccionJS/Js/22.js
+++ IntroduccionJS/IntroduccionJS/Js/22.js
@@ -1,1 +1,41 @@
 //estrucutras de control
+
+// if (condicion) {
+    //   //codigo a ejecutar si la condicion es verdadera
+    // } else {
+        // codigo a ejecutar si la condicion es falsa
+
+
+const puntaje = 1000; 
+
+if(puntaje ==1000) {
+    console.log("si el puntaje es 1000")
+}
+
+else {
+    console.log("no es 1000");
+}
+
+// si el puntaje es 1000 se le suma automaticamente un 1 al puntaje
+
+const efectivo = 1000;
+const carrito = 800;
+
+if(efectivo > carrito){
+    console.log("el usuario puede pagar")
+}
+else{
+    console.log("el usuario no tiene suficiente dinero para comprar")
+}
+
+
+const rol = "ADMINISTRADOR";
+
+if(rol === "ADMINISTRADOR"){
+    cpnsole.log("Acceso a todo el sistema");
+}else if(rol ==="EDITOR"){
+    console.log("Eres editor, peuedes entrar pero no puedes hacer mucho")
+}else{
+    console.log("No tienes acceso al sistema")
+} 
+;
---
+++ IntroduccionJS/IntroduccionJS/Js/23.js
@@ -0,0 +1,20 @@
+//switches en javascript
+// let day = "Monday";
+// switch (day) {
+    //     case "Sunday":
+    //     console.log("It's the weekend");
+    //break;
+
+const metodoPago = "Tarjeta";
+
+switch(metodoPago){
+    case "Efectivo":
+        console.log("Metodo de pago: Efectivo");
+    break;
+    case "Tarjeta":
+        console.log("Pagaste con Tarjeta");
+    break;
+    default:
+        console,log("Aun no has pagado");
+}
+
